a0a20927dd80f3417848ffb809c0fbaba81b33e3
ogabek <norpulatovogabek42@gmail.com>
2022-07-13 11:15:39 +0500

    df

--- bin/arithmetic018.dart
+++ bin/arithmetic018.dart
@@ -1,4 +1,9 @@
 // Create function func with arguments
 // the task condition is in the Readme file
+num func(int a, int b, int c) {
+  return (a + b + c) / 2;
+}
 
-void main() {}
+void main() {
+  print(func(5, 5, 5));
+}
