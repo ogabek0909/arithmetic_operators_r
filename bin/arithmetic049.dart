c579eb10a62a0431b8c02ad9ae22cdd558ffa3ae
ogabek <norpulatovogabek42@gmail.com>
2022-07-13 13:21:55 +0500

    adfsd

--- bin/arithmetic049.dart
+++ bin/arithmetic049.dart
@@ -1,4 +1,12 @@
 // Create function func with arguments
 // the task condition is in the Readme file
 
-void main() {}
+import 'dart:math';
+
+double func(int a, int b) {
+  return (a * b - 2 * b) * (a * b + (a / 2));
+}
+
+void main() {
+  print(func(2, 4));
+}
