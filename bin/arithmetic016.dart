5b9308f81f5022c6710a8a5516f5b37d8a85d651
ogabek <norpulatovogabek42@gmail.com>
2022-07-13 15:57:30 +0500

    sfdgsd

--- bin/arithmetic016.dart
+++ bin/arithmetic016.dart
@@ -1,7 +1,11 @@
 // Create function func with arguments
 // the task condition is in the Readme file
 double func(int a, int b) {
-  return -(a / (2 * b));
+  double x = -(a / (2 * b));
+  if (x < 13) {
+    return x = -2.5;
+  } else
+    return x = -4.0;
 }
 
 void main() {
