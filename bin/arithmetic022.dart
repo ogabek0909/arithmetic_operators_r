bd54a3e34e337acdee1aa88a45136aebf4f6a64d
ogabek <norpulatovogabek42@gmail.com>
2022-07-13 15:43:20 +0500

    dsf

--- bin/arithmetic022.dart
+++ bin/arithmetic022.dart
@@ -3,9 +3,13 @@
 import 'dart:math';
 
 num func(int a, int b, int c) {
-  return -(2 * pow(b, 2) - a) / (pow(a, 3) - pow(c, 2));
+  num x = -(2 * pow(b, 2) - a) / (pow(a, 3) - pow(c, 2));
+  if (x < 2) {
+    return -x * 100 ~/ 1 / 100;
+  } else
+    return x;
 }
 
 void main() {
-  print(func(1, 11, 3));
+  print(func(2, 4, 6));
 }
