9b8726debbbdb81058a1e3e0b69e0609c2888af2
ogabek <norpulatovogabek42@gmail.com>
2022-07-13 11:58:05 +0500

    sdf

--- bin/arithmetic017.dart
+++ bin/arithmetic017.dart
@@ -2,10 +2,10 @@
 // the task condition is in the Readme file
 import 'dart:math';
 
-num func(int a, int b) {
+double func(int a, int b) {
   return (pow(a, 2) + b) / (2 * pow(b, 2));
 }
 
 void main() {
-  print(func(5, 5));
+  print(func(3, 1));
 }
