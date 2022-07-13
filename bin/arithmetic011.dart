6272596a9d9b786b2b55ff032a19c10c8c2fc2c1
ogabek <norpulatovogabek42@gmail.com>
2022-07-13 10:58:58 +0500

    df

--- bin/arithmetic011.dart
+++ bin/arithmetic011.dart
@@ -1,7 +1,7 @@
 // Create function func with arguments
 // the task condition is in the Readme file
 int func(int a, int b) {
-  return (a + b) + (a - b);
+  return (a + b) * (a - b);
 }
 
 void main() {
